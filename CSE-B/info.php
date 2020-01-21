09068c8e686a39e8fdbffe8fa7d744b8255f7166
bhatia-samridhi <samridhibhatia3@gmail.com>
2020-01-21 14:57:42 +0530

    input webpage

---
+++ CSE-B/info.php
@@ -0,0 +1,9 @@
+<?php
+echo 'Welcome'.' '.$_GET['username'];
+echo "<br>";
+echo 'Age:'.$_GET['age'];
+echo "<br>";
+echo 'Contact:'.$_GET['contact'];
+echo "<br>";
+echo 'Email:'.$_GET['email'];
+?>
