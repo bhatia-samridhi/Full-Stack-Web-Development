fa6199d84666d1887e9c2744e6a9523ef93f5996
bhatia-samridhi <samridhibhatia3@gmail.com>
2020-01-21 14:57:01 +0530

    input webpage

---
+++ CSE-B/information.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form name="Form" action="info.php"  method="get">
+		
+  		Name: <input type="text" name="username" placeholder="Enter name"><br>
+   		Age: <input type="" name="age" placeholder="Enter age"><br>
+   		Contact:<input type="" name="contact" placeholder="Enter contact number"><br>
+   		Email:<input type="" name="email" placeholder="Enter email"><br>
+  		<input type="submit" name="btn">
+</form>
+
+</body>
+</html>
