8e50db821a384b257013e6033b05d22017ae8457
bhatia-samridhi <samridhibhatia3@gmail.com>
2020-01-22 14:24:08 +0530

    Javascript Code

--- CSE-B/information.php
+++ CSE-B/information.php
@@ -4,8 +4,8 @@
 	<title></title>
 </head>
 <body>
-<form name="Form" action="info.php"  method="get">
-		
+<form name="Form" action="info.php"  method="post">
+		Id: <input type="text" name="id" placeholder="Enter Id"><br>
   		Name: <input type="text" name="username" placeholder="Enter name"><br>
    		Age: <input type="" name="age" placeholder="Enter age"><br>
    		Contact:<input type="" name="contact" placeholder="Enter contact number"><br>
