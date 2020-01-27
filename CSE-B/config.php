8b830d7675dcfbd830f0a2541718467f36326a56
bhatia-samridhi <samridhibhatia3@gmail.com>
2020-01-27 11:30:17 +0530

    connection code

--- CSE-B/config.php
+++ CSE-B/config.php
@@ -3,13 +3,29 @@ $hostName = 'localhost';
 $username = 'root';
 $password = '';
 $databaseName = 'CSEB';
-
-if(mysqli_connect($hostName, $username, $password, $databaseName))
+$conn=mysqli_connect($hostName, $username, $password, $databaseName);
+if($conn)
 {
-	echo "Connection successful";
+	echo "Connection successful"."<br>";
 }
 else
 {
 	echo "Connection failed" + mysqli_connect_error();
+}
+if(isset($_POST['btn']))
+{
+	
+	$name = $_POST['username'];
+	$age = $_POST['age'];
+	$contact = $_POST['contact'];
+	$email = $_POST['email'];
+
+	$qy = "insert into users(name,age,contact,email)values('$name','$age','$contact','$email')";
+	$qt = mysqli_query($conn,$qy);
+	if($qt)
+	{
+		echo "<script>alert('registeration is complete')</script>";
+		echo"<script>window.open('information.php','self')</script>";
+	}
 }
 ?>
