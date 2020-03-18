2b37321ed96959c5136e8900f6c4dc55bdb22422
bhatia-samridhi <samridhibhatia3@gmail.com>
2020-03-18 22:13:38 +0530

    establishing connection with database

--- CSE-B/holi_assignment/connection.php
+++ CSE-B/holi_assignment/connection.php
@@ -10,4 +10,23 @@ if ($conn){
 else{
     echo "Connection failed!!" + mysqli_connect_error();
 }
+if(isset($_POST['btn']))
+{
+	
+	$enroll = $_POST['enroll_no'];
+ 	$name = $_POST['name'];
+ 	$age = $_POST['age'];
+ 	$course = $_POST['course'];
+ 	$branch = $_POST['branch'];
+ 	$contact = $_POST['contact'];
+ 	$email = $_POST['email'];
+
+	$qy = "insert into `student`(enroll_no,name,age,course,branch,contact,email)values(''$enroll', $name', '$age', '$course', '$branch', '$contact', '$email'')";
+	$qt = mysqli_query($conn,$qy);
+	if($qt)
+	{
+		echo "<script>alert('registeration is complete')</script>";
+		echo"<script>window.open('enterRecord.php','self')</script>";
+	}
+}
 ?>
