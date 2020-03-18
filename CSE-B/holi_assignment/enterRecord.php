a0e133b1fb0eedc9b61d747f3df95246b44355ca
bhatia-samridhi <samridhibhatia3@gmail.com>
2020-03-18 22:14:43 +0530

    webpage to enter record

--- CSE-B/holi_assignment/enterRecord.php
+++ CSE-B/holi_assignment/enterRecord.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <?php
-include('connection.php');
-?>
+include('connection.php');?>
 
 <html>
 <head>
 	<title></title>
 </head>
 <body>
-<form name="Form" action="connection.php"  method="post">
-		Enrollment No: <input type="text" name="id" placeholder="Enter Enrollment Number"><br>
-  		Name: <input type="text" name="username" placeholder="Enter Name"><br>
+<form name="Form" action="enterRecord.php"  method="post">
+		Enrollment No: <input type="text" name="enroll_no" placeholder="Enter Enrollment Number"><br>
+  		Name: <input type="text" name="name" placeholder="Enter Name"><br>
    		Age: <input type="" name="age" placeholder="Enter Age"><br>
-   		Course:<input type="" name="contact" placeholder="Enter Course"><br>
-   		Branch:<input type="" name="contact" placeholder="Enter Branch"><br>
+   		Course:<input type="" name="course" placeholder="Enter Course"><br>
+   		Branch:<input type="" name="branch" placeholder="Enter Branch"><br>
    		Contact:<input type="" name="contact" placeholder="Enter contact number"><br>
    		Email:<input type="" name="email" placeholder="Enter email"><br>
   		<input type="submit" name="btn">
