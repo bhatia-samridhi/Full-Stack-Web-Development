7aefc649bd69af5ea750c54ea9a73083664652b3
bhatia-samridhi <samridhibhatia3@gmail.com>
2020-03-18 21:03:51 +0530

    webpage to enter details

---
+++ CSE-B/holi_assignment/enterRecord.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<?php
+include('connection.php');
+?>
+
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form name="Form" action="config.php"  method="post">
+		Enrollment No: <input type="text" name="id" placeholder="Enter Enrollment Number"><br>
+  		Name: <input type="text" name="username" placeholder="Enter Name"><br>
+   		Age: <input type="" name="age" placeholder="Enter Age"><br>
+   		Course:<input type="" name="contact" placeholder="Enter Course"><br>
+   		Branch:<input type="" name="contact" placeholder="Enter Branch"><br>
+   		Contact:<input type="" name="contact" placeholder="Enter contact number"><br>
+   		Email:<input type="" name="email" placeholder="Enter email"><br>
+  		<input type="submit" name="btn">
+</form>
+
+</body>
+</html>
